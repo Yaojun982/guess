c994af99f576d2fa8f283253e4d10c3e53f96a0f
miia <miia@com.tw>
2019-10-29 16:43:16 +0800

    555

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tom.guess;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,9 +10,11 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,17 +22,24 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     EditText number;
+    int counter;
     TextView speak;
     TextView Please;
     int num;
     Random random = new Random();
     int secret = random.nextInt(10)+1;
+    private TextView edCounter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        Button button = findViewById(R.id.button);
         setSupportActionBar(toolbar);
+        edCounter = findViewById(R.id.counter);
+        edCounter.setText(counter + "");
         number = findViewById(R.id.number);
         speak = findViewById(R.id.speak);
 
@@ -43,13 +54,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void guess (View view){
-        num = Integer.parseInt(number.getText().toString());
+        int num = Integer.parseInt(number.getText().toString());
+        counter++;
+        edCounter.setText(counter + "");
+
         if (secret > num){
             speak.setText("bigger");
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("hahah")
+                    .setMessage("Biggr")
+                    .setPositiveButton("ok", null)
+                    .show();
         }else if(secret < num) {
               speak.setText("smaller");
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("hahah")
+                    .setMessage("smaller")
+                    .setPositiveButton("ok", null)
+                    .show();
         }else if(secret == num){
-             speak.setText("right");
+            speak.setText("right");
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("hahah")
+                    .setMessage("jo jo")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            secret = random.nextInt(10)+1;
+                            counter = 0;
+                        }
+                    })
+                    .show();
         }
     }
     @Override
